e657aa733955aa2826e0526db9574d655561be98
Margaret Diaz <margaret.diaz05@myhunter.cuny.edu>
2023-07-19 23:37:21 -0400

    update timeline site

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,15 +4,14 @@
 tmux kill-session -a
 
 # Change directory to your project folder
-cd /mnt/c/Users/marga/Desktop/MLH_FELLOW/Portfolio
+cd Portfolio
 
 # Update the git repository with the latest changes from the main branch on GitHub
 git fetch && git reset origin/main --hard
 
 # Enter the python virtual environment and install dependencies
-source /mnt/c/Users/marga/Desktop/MLH_FELLOW/python3-virtualenv/
+source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
 # Start a new detached Tmux session and run the Flask server
-tmux new-session -d -s flask-session 'cd /mnt/c/Users/marga/Desktop/MLH_FELLOW/Portfolio
- && source /mnt/c/Users/marga/Desktop/MLH_FELLOW/python3-virtualenv/ && flask run'
+# Make sure to run the Flask server on the VPS IP address!
