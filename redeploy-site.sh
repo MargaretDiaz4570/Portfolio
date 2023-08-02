00dea52f1749cf1d7c5c9a6d9e1a4cf788bbd447
Margaret Diaz <margaret.diaz05@myhunter.cuny.edu>
2023-08-02 01:29:46 -0400

    updated changes

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -8,10 +8,16 @@ cd Portfolio
 git fetch && git reset origin/main --hard
 
 # Enter the python virtual environment and install dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+#source python3-virtualenv/bin/activate
+#pip install -r requirements.txt
 
 # Restart myportfolio service
-sudo systemctl daemon-reload
-sudo systemctl restart myportfolio
-systemctl status myportfolio
+#sudo systemctl daemon-reload
+#sudo systemctl restart myportfolio
+#systemctl status myportfolio
+
+#Run `docker compose -f docker-compose.prod.yml down` (Let's first spin containers down to prevent out of memory issues on our VPS instances when building in the next step)
+docker compose -f docker-compose.prod.yml down
+
+#Run `docker compose -f docker-compose.prod.yml up -d --build`
+docker compose -f docker-compose.prod.yml up -d --build
