c5b74ed38a31cd618b50e076e18947f443a31437
Margaret Diaz <margaret.diaz05@myhunter.cuny.edu>
2023-08-13 01:06:07 -0400

    git action fix

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,7 @@
 
 
 # Make the script executable
-chmod +x $PWD/python3-virtualenv/bin/python
+chmod +x $PWD/python3-virtualenv/bin/python3
 
 # Run the tests
 $PWD/python3-virtualenv/bin/python -m unittest discover -v tests/
